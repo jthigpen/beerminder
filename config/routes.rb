314508fa7350663032b3566708364384069ce385
James Thigpen <james.r.thigpen@gmail.com>
2010-10-04 18:48:01 -0700

    Added admin seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Beerminder::Application.routes.draw do
+  get "home/index"
+
+  resource :user_session
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,10 +52,11 @@ Beerminder::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :controller => "home", :action => "index"
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+User.create!(:login => "admin", :password => "123qwe", :password_confirmation => "123qwe", :is_admin => true)
