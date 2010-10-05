2b126dd9c245c8d6f766e86b2bec9d2e61ea76d3
James Thigpen <james.r.thigpen@gmail.com>
2010-10-04 17:27:31 -0700

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beerminder::Application.config.secret_token = '8665a8301033a61243694db5f8247c8d201af719f25a161c7d983d8529a9c264a5935c9f55599840272ee8ef18cf7b08c04d5026b47a3e0c06a3255b117d5648'
